ea1ca681eed5a61c991427e2ad374e36d75e9bc6
pashyaa6255 <76003615+pashyaa6255@users.noreply.github.com>
2021-04-19 21:43:58 +0530

    My First Commit Two Divs Added"Pashyaa", "Pashyaa2222"

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = 
-`
+document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+<div>
+  This is Change Made By Coder Pashyaa....
+</div>
+<div>
+  This is Change Made By Coder Pashyaa2222....
 </div>
 `;
